42edae276743143f8cf6fea3084329afeb8c964d
BinaryVixen899 <lealac@msn.com>
2022-08-27 12:21:00 -0700

    Cleaned up use statements

--- Dolores/src/main.rs
+++ Dolores/src/main.rs
@@ -1,14 +1,14 @@
 use std::env; 
 
 use serenity::async_trait;
-
-use serenity::model::prelude::UserId;
-use serenity::model::user::User;
 use serenity::prelude::*;
+
 use serenity::model::channel::Message;
+use serenity::model::prelude::UserId;
 use serenity::model::gateway::Ready;
+
 use serenity::framework::standard::macros::{command, group};
-use serenity::framework::standard::{StandardFramework, CommandResult, Command};
+use serenity::framework::standard::{StandardFramework, CommandResult,};
 use serenity::http::Http;
 use serenity::utils::MessageBuilder;
 
@@ -31,51 +31,38 @@ impl EventHandler for Handler {
 async fn message(&self, context: Context, msg: Message){
 
         // Refactor all of this with a match statement
+  
 
-        if msg.content == "What does this Look like to you?" {
-            let response = MessageBuilder::new()
-        .push("Hello")
-        .push_bold_safe(&msg.author.name)
-        .push("welcome to Westworld!")
+      // You cannot just match on msg.content with a string because it's a struct field of type String
+      // This is why converting it to a String works, as well as why assigning its value to a variable works 
+       let responsemessage = match msg.content.as_str() {
+        "What does this look like to you" => String::from("Doesn't look like much of anything to me."),
+        "Mow" => String::from("Father, I think we're having Kieran for dinner tonight"),
+        "Skunk Noises" => String::from("No, she is too precious to eat"),
+        "Chirp" => String::from("Father, I think we're having Kauko for dinner!"),
+        "Yip Yap!" => String::from("Father, I think I've found dinner. And it's coyote.")
+        };
+        let response = MessageBuilder::new()
+        .push(responsemessage)
         .build();
-        }
 
-        // There must be some automatic conversion going on here 
-        if msg.content == "Mow" {
-            let response = MessageBuilder::new()
-            .push("Father, I think we're having Kieran for dinner tonight")
-            .build();
-        }
+    
+        // TODO: below command in msg.channel_id
+        // .push("Hello")
+        // .push_bold_safe(&msg.author.name)
+        // .push("welcome to Westworld!")
+        // .build();
+        // }
 
-        if msg.content == "Woof" {
-            let response = MessageBuilder::new()
-            .push("Father, I think we're having Kieran for dinner tonight")
-            .build();
-        }
+        // TODO: Ellen species and gender commands 
 
-        if msg.content == "Skunk Noises" {
-            let response = MessageBuilder::new()
-            .push("No, she is too precious to eat")
-            .build();
-        }
-
-        if msg.content == "Chirp" {
-            let response = MessageBuilder::new()
-            .push("Father, I think we're having Kauko for dinner!")
-            .build();
-        }
-
-        if msg.content == "Yip Yap!" {
-            let response = MessageBuilder::new()
-            .push("Father, I think I've found dinner. And it's coyote.")
-            .build();
-        }
 
    }
 
 
    async fn ready(&self, _: Context, ready: Ready) {
     // This is where we declare everything about the Bot (avatar, music, etc.)
+    // TODO: Right away we need to get the values of the channels for it to listen in 
     println!("{} is connected!", ready.user.name);
 }
 
@@ -133,6 +120,8 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("We failed, we failed to start listening for events: {:?}", why)
     }
+
+
     
 
     // TODO: At some point we need to tell it what server to connect to 
@@ -146,6 +135,7 @@ async fn EllenSpecies() -> CommandResult {
     todo!()
 }
 
+#[command]
 async fn EllenGender() -> CommandResult {
     todo!()
 }
